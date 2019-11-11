0caf02b2447a6fcad2a6b390c38c449b3b332d39
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-11 03:47:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,7 +53,6 @@ def reduce_to_all_true(source_array)
   while index < source_array.length do
   if new_array[index] == false || nil
     return false
-    break
   else pp true
     index += 1
 end
