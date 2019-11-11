8e11547013e53f1a1907d21f12947b2209c4df1e
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-11 03:37:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,8 +55,9 @@ def reduce_to_all_true(source_array)
       #new_array << source_array[index]
       #index += 1
     #else new_array = false
-    if source_array[index] == false 
+    if source_array[index] == false
       new_array == false
+      new_array
       break
     else
     new_array << source_array[index]
